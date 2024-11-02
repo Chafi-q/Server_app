b91e6e6063a02ac938e0bff7dc5ea5cc3db0cf4e
MohammedYassineBen <mohammed2021brainy@gmail.com>
2024-11-02 23:40:49 +0100

    ajout du microservice laboratoire

---
+++ Laboratoire/src/main/java/Repositories/LaboratoireRepository.java
@@ -0,0 +1,9 @@
+package Repositories;
+
+
+import entities.Laboratoire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaboratoireRepository extends JpaRepository<Laboratoire, Long> {
+   
+}
